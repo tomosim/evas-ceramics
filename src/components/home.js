0a1ebee0b011124ef6e8cf6a75c7cf58b710eec9
Tom Simmons <tom.simmons@erasys.de>
2023-08-01 22:17:52 +0200

    add styles to static images

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,62 @@
-import React from 'react';
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import * as styles from './home.module.css'
 
-const Home = ()=> {
-    return <>
-        <StaticImage src="../../static/images/Element3.png" alt="ceramic fragment" className={`${styles.image} ${styles.topRight}`} />
-        <StaticImage src="../../static/images/Element4.png" alt="ceramic fragment" className={`${styles.image} ${styles.bottomRight}`} />
-        <StaticImage src="../../static/images/Element2.png" alt="ceramic fragment" className={`${styles.image} ${styles.topLeft}`} />
-        <StaticImage src="../../static/images/Element1.png" alt="ceramic fragment" className={`${styles.image} ${styles.bottomLeft}`} />
-        <StaticImage src="../../static/images/Logo.png" alt="ceramic fragment" className={`${styles.image} ${styles.logo}`} />
+const Home = () => {
+  return (
+    <>
+      <StaticImage
+        src="../../static/images/Element3.png"
+        alt="ceramic fragment"
+        className={styles.topRight}
+        style={{
+          position: 'absolute',
+          overflow: 'hidden',
+          zIndex: '-1',
+        }}
+      />
+      <StaticImage
+        src="../../static/images/Element4.png"
+        alt="ceramic fragment"
+        className={styles.bottomRight}
+        style={{
+          position: 'absolute',
+          overflow: 'hidden',
+          zIndex: '-1',
+        }}
+      />
+      <StaticImage
+        src="../../static/images/Element2.png"
+        alt="ceramic fragment"
+        className={styles.topLeft}
+        style={{
+          position: 'absolute',
+          overflow: 'hidden',
+          zIndex: '-1',
+        }}
+      />
+      <StaticImage
+        src="../../static/images/Element1.png"
+        alt="ceramic fragment"
+        className={styles.bottomLeft}
+        style={{
+          position: 'absolute',
+          overflow: 'hidden',
+          zIndex: '-1',
+        }}
+      />
+      <StaticImage
+        src="../../static/images/Logo.png"
+        alt="ceramic fragment"
+        className={styles.logo}
+        style={{
+          position: 'absolute',
+          overflow: 'hidden',
+          zIndex: '-1',
+        }}
+      />
     </>
+  )
 }
 
-export default Home;
+export default Home
